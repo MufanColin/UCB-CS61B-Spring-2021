23dbf966198fae017f9b07d16f1c3f26e0cf25d3
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-06-13 23:00:39 +0800

    added guitarhero, TTFAF song is not very beautiful this time, I don't know why

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,45 @@
+package gh2;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class GuitarHero {
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    public static final int numKeys = 37;
+    public static final double CONCERT_A = 440.0;
+
+    public static void main(String[] args) {
+        GuitarString[] strings = new GuitarString[numKeys];
+        for (int i = 0; i < numKeys; i++) {
+            strings[i] = new GuitarString(CONCERT_A * Math.pow(2.0, (i - 24) * 1.0 / 12));
+        }
+        while (true) {
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                if (keyboard.contains(String.valueOf(key))) {
+                    int keyIndex = keyboard.indexOf(key);
+                    strings[keyIndex].pluck();
+
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (int i = 0; i < numKeys; i++) {
+                sample += strings[i].sample();
+            }
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < numKeys; i++) {
+                strings[i].tic();
+            }
+        }
+    }
+}
+
